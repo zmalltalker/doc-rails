78b411823a906de63977d3f6f973c05cd95f9d09
Xavier Noria <fxn@feynman.local>
2008-03-24 18:10:46 +0100

    documented Object#instance_values and #instance_variable_names

--- activesupport/lib/active_support/core_ext/object/instance_variables.rb
+++ activesupport/lib/active_support/core_ext/object/instance_variables.rb
@@ -6,6 +6,16 @@ class Object
     end
   end
 
+  # Returns a hash that maps instance variable names without "@" to their
+  # corresponding values. Keys are strings both in Ruby 1.8 and 1.9.
+  #
+  #   class C
+  #     def initialize(x, y)
+  #       @x, @y = x, y
+  #     end
+  #   end
+  #   
+  #   C.new(0, 1).instance_values # => {"x" => 0, "y" => 1}
   def instance_values #:nodoc:
     instance_variables.inject({}) do |values, name|
       values[name.to_s[1..-1]] = instance_variable_get(name)
@@ -13,6 +23,16 @@ class Object
     end
   end
 
+  # Returns an array of instance variable names including "@". They are strings
+  # both in Ruby 1.8 and 1.9.
+  #
+  #   class C
+  #     def initialize(x, y)
+  #       @x, @y = x, y
+  #     end
+  #   end
+  #   
+  #   C.new(0, 1).instance_variable_names # => ["@y", "@x"]
   def instance_variable_names
     instance_variables.map(&:to_s)
   end
