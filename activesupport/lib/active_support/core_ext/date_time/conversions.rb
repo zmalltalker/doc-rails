zmalltalker/doc-rails

commit 3caee7539d149e0382a447c82a3891b4eb897c60

Sync with main rails

activesupport/lib/active_support/core_ext/time/conversions.rb
@@ -20,12 +20,11 @@ module ActiveSupport #:nodoc:
           end
         end
 
-        # Converts to a formatted string. See Time::DATE_FORMATS for builtin formats.
+        # Convert to a formatted string. See DATE_FORMATS for builtin formats.
         #
         # This method is aliased to <tt>to_s</tt>.
         #
-        # ==== Examples
-        #
+        # ==== Examples:
         #   time = Time.now                     # => Thu Jan 18 06:10:17 CST 2007
         #
         #   time.to_formatted_s(:time)          # => "06:10:17"
@@ -37,8 +36,7 @@ module ActiveSupport #:nodoc:
         #   time.to_formatted_s(:long_ordinal)  # => "January 18th, 2007 06:10"
         #   time.to_formatted_s(:rfc822)        # => "Thu, 18 Jan 2007 06:10:17 -0600"
         #
-        # == Adding your own time formats to <tt>to_formatted_s</tt>
-        #
+        # == Adding your own time formats to to_formatted_s
         # You can add your own formats to the Time::DATE_FORMATS hash.
         # Use the format name as the hash key and either a strftime string
         # or Proc instance that takes a time argument as the value.
@@ -51,7 +49,7 @@ module ActiveSupport #:nodoc:
           formatter.respond_to?(:call) ? formatter.call(self).to_s : strftime(formatter)
         end
         
-        # Returns the UTC offset as an +HH:MM formatted string.
+        # Returns the utc_offset as an +HH:MM formatted string. Examples:
         #
         #   Time.local(2000).formatted_offset         # => "-06:00"
         #   Time.local(2000).formatted_offset(false)  # => "-0600"
@@ -59,15 +57,20 @@ module ActiveSupport #:nodoc:
           utc? && alternate_utc_string || utc_offset.to_utc_offset_s(colon)
         end
 
-        # Converts a Time object to a Date, dropping hour, minute, and second precision.
+        # Convert a Time object to a Date, dropping hour, minute, and second precision.
         #
         # ==== Examples
+        #   my_time = Time.now
+        #   # => Mon Nov 12 22:59:51 -0500 2007
         #
-        #   my_time = Time.now # => Mon Nov 12 22:59:51 -0500 2007
-        #   my_time.to_date    # => Mon, 12 Nov 2007
+        #   my_time.to_date
+        #   #=> Mon, 12 Nov 2007
         #
-        #   your_time = Time.parse("1/13/2009 1:13:03 P.M.") # => Tue Jan 13 13:13:03 -0500 2009
-        #   your_time.to_date                                # => Tue, 13 Jan 2009
+        #   your_time = Time.parse("1/13/2009 1:13:03 P.M.")
+        #   # => Tue Jan 13 13:13:03 -0500 2009
+        #
+        #   your_time.to_date
+        #   # => Tue, 13 Jan 2009
         def to_date
           ::Date.new(year, month, day)
         end
@@ -81,12 +84,17 @@ module ActiveSupport #:nodoc:
         # Converts a Time instance to a Ruby DateTime instance, preserving UTC offset.
         #
         # ==== Examples
+        #   my_time = Time.now
+        #   # => Mon Nov 12 23:04:21 -0500 2007
         #
-        #   my_time = Time.now  # => Mon Nov 12 23:04:21 -0500 2007
-        #   my_time.to_datetime # => Mon, 12 Nov 2007 23:04:21 -0500
+        #   my_time.to_datetime
+        #   # => Mon, 12 Nov 2007 23:04:21 -0500
         #
-        #   your_time = Time.parse("1/13/2009 1:13:03 P.M.") # => Tue Jan 13 13:13:03 -0500 2009
-        #   your_time.to_datetime                            # => Tue, 13 Jan 2009 13:13:03 -0500
+        #   your_time = Time.parse("1/13/2009 1:13:03 P.M.")
+        #   # => Tue Jan 13 13:13:03 -0500 2009
+        #
+        #   your_time.to_datetime
+        #   # => Tue, 13 Jan 2009 13:13:03 -0500
         def to_datetime
           ::DateTime.civil(year, month, day, hour, min, sec, Rational(utc_offset, 86400))
         end

activesupport/lib/active_support/core_ext/time/zones.rb
@@ -17,9 +17,10 @@ module ActiveSupport #:nodoc:
 
           # Sets a global default time zone, separate from the system time zone in ENV['TZ']. 
           # Accepts either a Rails TimeZone object, a string that identifies a 
-          # Rails TimeZone object (e.g., "Central Time (US & Canada)"), or a TZInfo::Timezone object.
+          # Rails TimeZone object (e.g., "Central Time (US & Canada)"), or a TZInfo::Timezone object
           #
-          # Any Time or DateTime object can use this default time zone, via <tt>in_time_zone</tt>.
+          # Any Time or DateTime object can use this default time zone, via #in_time_zone.
+          # Example:
           #
           #   Time.zone = 'Hawaii'          # => 'Hawaii'
           #   Time.utc(2000).in_time_zone   # => Fri, 31 Dec 1999 14:00:00 HST -10:00
@@ -27,8 +28,7 @@ module ActiveSupport #:nodoc:
             Thread.current[:time_zone] = get_zone(time_zone)
           end
           
-          # Allows override of Time.zone locally inside supplied block; resets
-          # Time.zone to existing value when done.
+          # Allows override of Time.zone locally inside supplied block; resets Time.zone to existing value when done
           def use_zone(time_zone)
             old_zone, ::Time.zone = ::Time.zone, get_zone(time_zone)
             yield

activesupport/lib/active_support/core_ext/array/conversions.rb
@@ -46,7 +46,7 @@ module ActiveSupport #:nodoc:
             alias_method :to_s, :to_formatted_s
           end
         end
-        
+
         # Converts a collection of elements into a formatted string by calling
         # <tt>to_s</tt> on all elements and joining them:
         #
@@ -68,7 +68,7 @@ module ActiveSupport #:nodoc:
               to_default_s
           end
         end
-        
+
         # Returns a string that represents this array in XML by sending
         # <tt>to_xml</tt> to each element.
         #

activesupport/lib/active_support/core_ext/bigdecimal/conversions.rb
@@ -1,30 +1,41 @@
 require 'yaml'
 
-class BigDecimal #:nodoc:
-  alias :_original_to_s :to_s
-  
-  def to_s(format="F")
-    _original_to_s(format)
-  end
-
-  yaml_as "tag:yaml.org,2002:float"
-  def to_yaml( opts = {} )
-    YAML::quick_emit( nil, opts ) do |out|
-      # This emits the number without any scientific notation.
-      # I prefer it to using self.to_f.to_s, which would lose precision.
-      #
-      # Note that YAML allows that when reconsituting floats
-      # to native types, some precision may get lost.
-      # There is no full precision real YAML tag that I am aware of.
-      str = self.to_s
-      if str == "Infinity"
-        str = ".Inf"
-      elsif str == "-Infinity"
-        str = "-.Inf"
-      elsif str == "NaN"
-        str = ".NaN"
+module ActiveSupport #:nodoc:
+  module CoreExtensions #:nodoc:
+    module BigDecimal #:nodoc:
+      module Conversions
+        def self.included(base) #:nodoc:
+          base.instance_eval do
+            alias_method :_original_to_s, :to_s
+            alias_method :to_s, :to_formatted_s
+          end
+        end
+        
+        def to_formatted_s(format="F")
+          _original_to_s(format)
+        end
+        
+        yaml_as "tag:yaml.org,2002:float"
+        def to_yaml( opts = {} )
+          YAML::quick_emit( nil, opts ) do |out|
+            # This emits the number without any scientific notation.
+            # I prefer it to using self.to_f.to_s, which would lose precision.
+            #
+            # Note that YAML allows that when reconsituting floats
+            # to native types, some precision may get lost.
+            # There is no full precision real YAML tag that I am aware of.
+            str = self.to_s
+            if str == "Infinity"
+              str = ".Inf"
+            elsif str == "-Infinity"
+              str = "-.Inf"
+            elsif str == "NaN"
+              str = ".NaN"
+            end
+            out.scalar( "tag:yaml.org,2002:float", str, :plain )
+          end
+        end
       end
-      out.scalar( "tag:yaml.org,2002:float", str, :plain )
     end
   end
 end

commit edbf3efd6552c63bef8fc8dfa401b85d8ba492b3

Applying doc regressions from 3caee753 sync.

activesupport/lib/active_support/core_ext/date_time/conversions.rb
@@ -44,7 +44,7 @@ module ActiveSupport #:nodoc:
           formatter.respond_to?(:call) ? formatter.call(self).to_s : strftime(formatter)
         end
 
-        # Returns the utc_offset as an +HH:MM formatted string. Examples:
+        # Returns the +utc_offset+ as an +HH:MM formatted string. Examples:
         #
         #   datetime = DateTime.civil(2000, 1, 1, 0, 0, 0, Rational(-6, 24))
         #   datetime.formatted_offset         # => "-06:00"

activesupport/lib/active_support/core_ext/string/unicode.rb
@@ -10,11 +10,11 @@ module ActiveSupport #:nodoc:
           # string overrides can also be called through the +chars+ proxy.
           #
           #   name = 'Claus Müller'
-          #   name.reverse #=> "rell??M sualC"
-          #   name.length #=> 13
+          #   name.reverse  #=> "rell??M sualC"
+          #   name.length   #=> 13
           #
-          #   name.chars.reverse.to_s #=> "rellüM sualC"
-          #   name.chars.length #=> 12
+          #   name.chars.reverse.to_s   #=> "rellüM sualC"
+          #   name.chars.length         #=> 12
           #   
           #
           # All the methods on the chars proxy which normally return a string will return a Chars object. This allows

activesupport/lib/active_support/core_ext/time/conversions.rb
@@ -20,11 +20,10 @@ module ActiveSupport #:nodoc:
           end
         end
 
-        # Convert to a formatted string. See DATE_FORMATS for builtin formats.
+        # Converts to a formatted string. See DATE_FORMATS for builtin formats.
         #
         # This method is aliased to <tt>to_s</tt>.
         #
-        # ==== Examples:
         #   time = Time.now                     # => Thu Jan 18 06:10:17 CST 2007
         #
         #   time.to_formatted_s(:time)          # => "06:10:17"
@@ -36,7 +35,7 @@ module ActiveSupport #:nodoc:
         #   time.to_formatted_s(:long_ordinal)  # => "January 18th, 2007 06:10"
         #   time.to_formatted_s(:rfc822)        # => "Thu, 18 Jan 2007 06:10:17 -0600"
         #
-        # == Adding your own time formats to to_formatted_s
+        # == Adding your own time formats to +to_formatted_s+
         # You can add your own formats to the Time::DATE_FORMATS hash.
         # Use the format name as the hash key and either a strftime string
         # or Proc instance that takes a time argument as the value.
@@ -49,7 +48,7 @@ module ActiveSupport #:nodoc:
           formatter.respond_to?(:call) ? formatter.call(self).to_s : strftime(formatter)
         end
         
-        # Returns the utc_offset as an +HH:MM formatted string. Examples:
+        # Returns the +utc_offset+ as an +HH:MM formatted string.
         #
         #   Time.local(2000).formatted_offset         # => "-06:00"
         #   Time.local(2000).formatted_offset(false)  # => "-0600"
@@ -57,20 +56,13 @@ module ActiveSupport #:nodoc:
           utc? && alternate_utc_string || utc_offset.to_utc_offset_s(colon)
         end
 
-        # Convert a Time object to a Date, dropping hour, minute, and second precision.
+        # Converts a Time object to a Date, dropping hour, minute, and second precision.
         #
-        # ==== Examples
-        #   my_time = Time.now
-        #   # => Mon Nov 12 22:59:51 -0500 2007
+        #   my_time = Time.now  # => Mon Nov 12 22:59:51 -0500 2007
+        #   my_time.to_date     #=> Mon, 12 Nov 2007
         #
-        #   my_time.to_date
-        #   #=> Mon, 12 Nov 2007
-        #
-        #   your_time = Time.parse("1/13/2009 1:13:03 P.M.")
-        #   # => Tue Jan 13 13:13:03 -0500 2009
-        #
-        #   your_time.to_date
-        #   # => Tue, 13 Jan 2009
+        #   your_time = Time.parse("1/13/2009 1:13:03 P.M.")  # => Tue Jan 13 13:13:03 -0500 2009
+        #   your_time.to_date                                 # => Tue, 13 Jan 2009
         def to_date
           ::Date.new(year, month, day)
         end
@@ -83,18 +75,11 @@ module ActiveSupport #:nodoc:
 
         # Converts a Time instance to a Ruby DateTime instance, preserving UTC offset.
         #
-        # ==== Examples
-        #   my_time = Time.now
-        #   # => Mon Nov 12 23:04:21 -0500 2007
+        #   my_time = Time.now    # => Mon Nov 12 23:04:21 -0500 2007
+        #   my_time.to_datetime   # => Mon, 12 Nov 2007 23:04:21 -0500
         #
-        #   my_time.to_datetime
-        #   # => Mon, 12 Nov 2007 23:04:21 -0500
-        #
-        #   your_time = Time.parse("1/13/2009 1:13:03 P.M.")
-        #   # => Tue Jan 13 13:13:03 -0500 2009
-        #
-        #   your_time.to_datetime
-        #   # => Tue, 13 Jan 2009 13:13:03 -0500
+        #   your_time = Time.parse("1/13/2009 1:13:03 P.M.")  # => Tue Jan 13 13:13:03 -0500 2009
+        #   your_time.to_datetime                             # => Tue, 13 Jan 2009 13:13:03 -0500
         def to_datetime
           ::DateTime.civil(year, month, day, hour, min, sec, Rational(utc_offset, 86400))
         end

activesupport/lib/active_support/core_ext/time/zones.rb
@@ -17,10 +17,9 @@ module ActiveSupport #:nodoc:
 
           # Sets a global default time zone, separate from the system time zone in ENV['TZ']. 
           # Accepts either a Rails TimeZone object, a string that identifies a 
-          # Rails TimeZone object (e.g., "Central Time (US & Canada)"), or a TZInfo::Timezone object
+          # Rails TimeZone object (e.g., "Central Time (US & Canada)"), or a TZInfo::Timezone object.
           #
-          # Any Time or DateTime object can use this default time zone, via #in_time_zone.
-          # Example:
+          # Any Time or DateTime object can use this default time zone, via <tt>in_time_zone</tt>.
           #
           #   Time.zone = 'Hawaii'          # => 'Hawaii'
           #   Time.utc(2000).in_time_zone   # => Fri, 31 Dec 1999 14:00:00 HST -10:00