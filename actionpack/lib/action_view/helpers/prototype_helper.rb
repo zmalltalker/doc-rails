ed56ba2f42ffdaec91c14d3fb0fe1982d91bc7a1
Jérôme <jerome@yayel.com>
2008-03-28 17:18:26 +0100

    syntax fix for insert_html and replace doc

--- actionpack/lib/action_view/helpers/prototype_helper.rb
+++ actionpack/lib/action_view/helpers/prototype_helper.rb
@@ -716,11 +716,11 @@ module ActionView
           #   # Insert the rendered 'navigation' partial just before the DOM
           #   # element with ID 'content'.
           #   # Generates: new Insertion.Before("content", "-- Contents of 'navigation' partial --");
-          #   insert_html :before, 'content', :partial => 'navigation'
+          #   page.insert_html :before, 'content', :partial => 'navigation'
           #
           #   # Add a list item to the bottom of the <ul> with ID 'list'.
           #   # Generates: new Insertion.Bottom("list", "<li>Last item</li>");
-          #   insert_html :bottom, 'list', '<li>Last item</li>'
+          #   page.insert_html :bottom, 'list', '<li>Last item</li>'
           #
           def insert_html(position, id, *options_for_render)
             insertion = position.to_s.camelize
@@ -749,7 +749,7 @@ module ActionView
           #
           #   # Replace the DOM element having ID 'person-45' with the
           #   # 'person' partial for the appropriate object.
-          #   replace 'person-45', :partial => 'person', :object => @person
+          #   page.replace 'person-45', :partial => 'person', :object => @person
           #
           # This allows the same partial that is used for the +insert_html+ to
           # be also used for the input to +replace+ without resorting to
